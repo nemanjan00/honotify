48976291332b01714adb6eb86804c68eb57c41ec
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-07-25 14:27:43 +0200

    Improve logging

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const net = require('net');
 const spawn = require("child_process").spawn;
 
 let verbose = false;
+let ip = "0.0.0.0";
+let port = 5900;
 
 const server = net.createServer((socket) => {
 	socket.on("error", (error) => {
@@ -14,7 +16,22 @@ const server = net.createServer((socket) => {
 
 	socket.end();
 
+	console.log("You are being scanned by " + address);
 	spawn("notify-send", ["--icon", "/usr/share/icons/Adwaita/64x64/status/dialog-warning-symbolic.symbolic.png", "Honotify", "You are being scanned by " + address]);
+});
+
+server.on("listening", () => {
+	console.log("Succesfully started listening on "+ip+":"+port);
+})
+
+server.on('error', (e) => {
+	if (e.code === 'EADDRINUSE') {
+		console.log('Address in use, retrying...');
+		setTimeout(() => {
+			server.close();
+			server.listen(port, ip);
+		}, 1000);
+	}
 });
 
 server.listen(5900, '0.0.0.0');
